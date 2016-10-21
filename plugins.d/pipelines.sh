0be5821803f2a2c32f4f38529f53ba882d8b4bb8
Bjarte Stien Karlsen <bjarte@bjartek.org>
2016-10-21 23:39:52 +0200

    added comments

--- plugins.d/pipelines.sh
+++ plugins.d/pipelines.sh
@@ -3,6 +3,10 @@
 plugin_pipelines() {
 	local profile_dir=$1
 
+	#in order to add commands that should be run after oc cluster up is done append to $profile_dir/run_after
+	#if you want to add arguments to oc cluster add them to $profile_dir/run
+	#I guess another strategy to use her is to copy another SH file to $profile_dir and just add a source and function call in the HEREDOCS below
+
 	cat << EOF > $profile_dir/run_after
 echo "Enabling pipelines plugin"
 sed -i 's/  extensionScripts: null/  extensionScripts:\\
@@ -13,4 +17,5 @@ EOF
   RESTART_CLUSTER=true
 }
 
+#this plugin adds a flag to the UP command. so we add the flag name to the flags folder in dispatch dir
 echo "plugin_pipelines" > $DISPATCH_DIR/flags/--with-pipelines
--- plugins.d/test.sh
+++ plugins.d/test.sh
@@ -5,4 +5,5 @@ test_foo() {
 	echo "I am a test"
 }
 
+#here we add the test command and say that it should execute the test_foo function above
 echo "test_foo" > $DISPATCH_DIR/test
